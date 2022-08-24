3135e11ba64723d0de03626a584b26d97486784a
Damian Suess <djsuess@gmail.com>
2022-08-24 12:29:11 -0400

    Moved ctrl to its own project

--- souce/ViewModels/ShellWindowViewModel.cs
+++ souce/ViewModels/ShellWindowViewModel.cs
@@ -1,10 +1,32 @@
-﻿namespace Learn.AvaloniaProgressRing.ViewModels
+﻿using Prism.Commands;
+
+namespace Learn.AvaloniaProgressRing.ViewModels
 {
   public class ShellWindowViewModel : ViewModelBase
   {
+    private bool _isLoading;
+
     public ShellWindowViewModel()
     {
       Title = "Sample Prism.Avalonia - Navigation";
+
+      IsLoading = true;
+    }
+
+    DelegateCommand CmdStart => new DelegateCommand(() =>
+    {
+      IsLoading = true;
+    });
+
+    DelegateCommand CmdStop => new DelegateCommand(() =>
+    {
+      IsLoading = false;
+    });
+
+    public bool IsLoading
+    {
+      get => _isLoading;
+      set => SetProperty(ref _isLoading, value);
     }
   }
 }
